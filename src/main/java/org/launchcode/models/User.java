0aa9c982c7e45dd890a50cb9fef0f9f42b6127a5
E J <johnsonelijah440@gmail.com>
2024-01-25 20:00:05 -0600

    <Studio>

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,18 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotEmpty
+    @Size(min = 5, max = 15, message = "Username must be between 5-15 characters")
     private String username;
+    @Email(message = "Must input a valid email")
     private String email;
+    @NotEmpty
+    @Size(min = 6, message = "Password must be longer than 6 characters")
     private String password;
 
     public User() {
